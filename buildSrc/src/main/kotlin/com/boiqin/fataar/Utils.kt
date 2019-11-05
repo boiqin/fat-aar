876242902679309acc7570cb37f377fd5689f451
boji2009 <cbasc1989@qq.com>
2019-11-06 00:06:56 +0800

    [update]修改版本比较算法

--- buildSrc/src/main/kotlin/com/boiqin/fataar/Utils.kt
+++ buildSrc/src/main/kotlin/com/boiqin/fataar/Utils.kt
@@ -11,8 +11,7 @@ import kotlin.math.min
  * @author kezong @since 2018-12-10 17:28
  * modify by alexbchen on 2019/10/31
  */
-class Utils {
-    companion object {
+object Utils {
         private var projectRef: WeakReference<Project>? = null
 
         fun setProject(project: Project) {
@@ -20,55 +19,49 @@ class Utils {
         }
 
         fun logError(msg: String) {
-            val p = projectRef?.get()
-            p?.logger?.error("[fat-aar]${msg}")
+            val project = projectRef?.get()
+            project?.logger?.error("fataar-${msg}")
         }
 
         fun logInfo(msg: String) {
             val p = projectRef?.get()
-            p?.logger?.info("[fat-aar]${msg}")
+            p?.logger?.info("fataar-${msg}")
         }
 
         fun logAnytime(msg: String) {
-            //TODO
             val p = projectRef?.get()
             p?.logger?.debug("[fat-aar]${msg}")
         }
 
-        fun showDir(indent: Int, file: File) {
-            for (i in 0..indent.minus(1)) {
-                print('-')
-            }
-            println(file.name + " " + file.length())
-            if (file.isDirectory) {
-                val files = file.listFiles()
-                files.forEach {
-                    showDir(indent + 4, it)
-                }
-            }
-        }
-
         fun compareVersion(v1: String, v2: String): Int {
-
-            val versionArray1 = v1.split("\\.")
-            val versionArray2 = v2.split("\\.")
-            var idx = 0
-            val minLength = min(versionArray1.size, versionArray2.size)
-            var diff = 0
-            while (idx < minLength) {
-                diff = versionArray1[idx].length - versionArray2[idx].length//先比较长度
-                if (diff != 0) {
-                    break
+            val s1 = v1.split(".")
+            val s2 = v2.split(".")
+            val len1 = s1.size
+            val len2 = s2.size
+            var i = 0
+            var j = 0
+            while (i < len1 && j < len2) {
+                if (s1[i].toInt() > s2[j].toInt()) {
+                    return 1
+                } else if (s1[i].toInt() < s2[j].toInt()) {
+                    return -1
                 }
-                diff = versionArray1[idx].compareTo(versionArray2[idx])//再比较字符
-                if (diff != 0) {
-                    break
-                }
-                ++idx
+                i++
+                j++
             }
-            //如果已经分出大小，则直接返回，如果未分出大小，则再比较位数，有子版本的为大；
-            diff = if (diff != 0) diff else (versionArray1.size - versionArray2.size)
-            return diff
+            while (i < len1) {
+                if (s1[i].toInt() != 0) {
+                    return 1
+                }
+                i++
+            }
+            while (j < len2) {
+                if (s2[j].toInt() != 0) {
+                    return -1
+                }
+                j++
+            }
+            return 0
 
         }
 
@@ -97,7 +90,11 @@ class Utils {
             return !this.exists() || !this.isDirectory
         }
 
-
+    fun main() {
+        println(compareVersion("3.5.0","3.4.0"))
+        println(compareVersion("3.5.0","3.4"))
+        println(compareVersion("3.3.0","3.4"))
+        println(compareVersion("3.3","3.4"))
+        println(compareVersion("2.1.4","3.4"))
     }
-
 }
